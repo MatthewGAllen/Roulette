66e9cd57fb0a5f3578eae8c5b893e55528c47226
Pedro Marchante <pmarc560@gmail.com>
2019-08-19 19:06:20 -0400

    Implemented a continuous play mode

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -6,17 +6,22 @@ namespace Roulette
     {
         static void Main(string[] args)
         {
-            GameBoard g = new GameBoard();
-            
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                Console.Clear();
+                GameBoard g = new GameBoard();
 
-            g.IsColor();
-            g.EvenOrOdd();
-            g.WinningNumber();
-            g.LowHigh();
-            g.Thirds();
-            g.Column();
-            g.Row();
-            g.TwoRows();
+
+                g.IsColor();
+                g.EvenOrOdd();
+                g.WinningNumber();
+                g.LowHigh();
+                g.Thirds();
+                g.Column();
+                g.Row();
+                g.TwoRows();
+                Console.WriteLine("\nPress enter to play again or escape to exit\n");
+            }
         }
     }
 }
