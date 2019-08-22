8288655fc240bf94f8367a23d4da579280ede4c8
arpethel <arpethel@gmail.com>
2019-08-22 14:30:58 -0400

    Restructured the behavior of ball class in methods

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -8,11 +8,14 @@ namespace Roulette
         {
             Player p = new Player(1000);
             GameBoard g = new GameBoard(p);
+
+            Console.WriteLine("Let's Play! \nType 'red' or 'black', \n'even' or 'odd', \n'low' or 'high', \n'column', \n'thirds', \n'row', \n'two rows'");
             Console.WriteLine("Press any key to start game");
             string input;
             while (Console.ReadKey().Key != ConsoleKey.Escape)
             {
                 Console.Clear();
+                Ball b = new Ball();
 
                 Console.WriteLine("Let's Play! \nType 'red' or 'black', \n'even' or 'odd', \n'low' or 'high', \n'column', \n'thirds', \n'row', \n'two rows'");
                 Console.WriteLine("What kind of bet would you like to make?");
@@ -21,25 +24,22 @@ namespace Roulette
                 switch (input)
                 {
                     case "red":
-                        g.IsColor(input);
+                        g.IsColor(b, input);
                         break;
 
                     case "black":
-                        g.IsColor(input);
+                        g.IsColor(b, input);
                         break;
 
                     case "even":
-                        g.EvenOrOdd(input);
+                        g.EvenOrOdd(b, input);
                         break;
 
                     case "odd":
-                        g.EvenOrOdd(input);
+                        g.EvenOrOdd(b, input);
                         break;
 
-                    case "help":
-                        Console.WriteLine("Available bets are: [red or black],[even or odd],[low or high],[column]" +
-                            ",[thirds],[row],[two rows],");
-                        break;
+
                 }
 
                 if (input != "help")
