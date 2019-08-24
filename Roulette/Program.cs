e0c4681b162eacc9af084c809736029009336e5f
Matthew <Allenm40@my.erau.edu>
2019-08-23 20:54:07 -0400

    Finished the switch statements and fixed the way that the user leaves the game

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -9,17 +9,20 @@ namespace Roulette
             Player p = new Player(1000);
             GameBoard g = new GameBoard(p);
 
-            Console.WriteLine("Let's Play! \nType 'red' or 'black', \n'even' or 'odd', \n'low' or 'high', \n'column', \n'thirds', \n'row', \n'two rows'");
-            Console.WriteLine("Press any key to start game");
+            
+            bool truther = true;
             string input;
-            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            while (truther)
             {
+                
                 Console.Clear();
                 Ball b = new Ball();
 
-                Console.WriteLine("Let's Play! \nType 'red' or 'black', \n'even' or 'odd', \n'low' or 'high', \n'column', \n'thirds', \n'row', \n'two rows'");
+                Console.WriteLine("Let's Play! \nType 'red' or 'black', \n'even' or 'odd', \n'low high', \n'column'," +
+                    " \n'thirds', \n'row', \n'two rows', \n'split', \n'corner', \n'number',");
                 Console.WriteLine("What kind of bet would you like to make?");
                 input = Console.ReadLine();
+                
 
                 switch (input)
                 {
@@ -44,12 +47,35 @@ namespace Roulette
                     case "split":
                         g.SplitBet(b);
                         break;
-
+                    case "number":
+                        g.WinningNumber(b);
+                        break;
+                    case "row":
+                        g.Row(b);
+                        break;
+                    case "two rows":
+                        g.TwoRows(b);
+                        break;
+                    case "thirds":
+                        g.Thirds(b);
+                        break;
+                    case "low high":
+                        g.LowHigh(b);
+                        break;
+                    case "column":
+                        g.Column(b);
+                        break;
 
                 }
 
-                if (input != "help")
-                    Console.WriteLine("\nPress enter to play again or escape to exit\n");
+                
+                Console.WriteLine("\nPress enter to play again or escape to exit\n");
+                
+                if(Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    truther = false;
+                }
+
             }
         }
     }
